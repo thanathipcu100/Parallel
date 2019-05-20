76559888cf7747538ba25a60b4130f37ec009761
iz.actxe <imperialzz.actxe@gmail.com>
2019-05-20 23:48:53 +0700

    Complete Project

--- backend/app.js
+++ backend/app.js
@@ -9,17 +9,15 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/dissys_miniproject');
 
 var indexController = require('./controllers/index');
-
 var app = express();
 
-// uncomment after placing your favicon in /public
+//uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexController);
 
 // catch 404 and forward to error handler
@@ -34,7 +32,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,4 +1,4 @@
-var express = require('express');
+﻿var express = require('express');
 var router = express.Router();
 var User = require('../models/user.js');
 var Group = require('../models/group.js');
@@ -7,20 +7,16 @@ var Join = require('../models/join.js');
 
 /* GET home page. */
 
-// -------------
-// --- users ---
-// -------------
-
-// body: [name (string)]
-// Result: uid (objectId)
-router.post('/auth', function (req, res) {
+// body: [name(string)]
+// Result: uid(objectId)
+router.post('/auth',function(req,res) {
   var query = { name: req.body.name };
-  User.find(query, function (err, users) {
-    if (err) throw err
-    else if (users.length == 0) {
+  User.find(query,function(err,users) {
+    if(err) throw err
+    else if(users.length == 0) {
       var user_model = new User(query);
-      user_model.save(function (err, userss) {
-        if (err) throw err
+      user_model.save(function(err,userss) {
+        if(err) throw err
         return res.send({ "uid": userss.id });
       });
     }
@@ -28,10 +24,10 @@ router.post('/auth', function (req, res) {
   });
 });
 
-// query: [uid (objectId)]
+// query: [uid(objectId)]
 // result: groups (array of object)
-router.get('/getUserInfo', function (req, res) {
-  Join.find({ uid: req.query.uid }, function (err, joins) {
+router.get('/getUserInfo',function (req, res) {
+  Join.find({ uid: req.query.uid }, function (err,joins) {
     if(err) {
       console.error(err);
       res.send({groups: []});
@@ -39,11 +35,11 @@ router.get('/getUserInfo', function (req, res) {
     else {
       var result = [];
       var promises;
-      promises = joins.map((join, index) =>
+      promises = joins.map((join,index) =>
         Group.find({
           _id: join.gid
         }).then(function (groups) {
-          if (groups.length) result.push(groups[0]);
+          if(groups.length) result.push(groups[0]);
         })
       );
 
@@ -57,12 +53,15 @@ router.get('/getUserInfo', function (req, res) {
   });
 });
 
+// ------------
+// --- chat ---
+// ------------
 
 // body: [uid (objectId), gname (string)]
 // result: gid (objectId)
-router.post('/createGroup', function (req, res) {
+router.post('/createGroup',function(req,res) {
   var query = { name: req.body.gname };
-  Group.find(query, function (err, groups) {
+  Group.find(query,function(err,groups) {
     if (err) console.error("group finding error");
     else if (groups.length == 0) {
       var group_model = new Group(query);
@@ -70,7 +69,7 @@ router.post('/createGroup', function (req, res) {
         if (err) throw err;
         var join_model = new Join({ uid: req.body.uid, gid: group.id, read_at: 0 });
         join_model.save(function (err) {
-          if (err) throw err;
+          if(err) throw err;
           return res.send({ "gid": group.id });
         })
       })
@@ -81,17 +80,17 @@ router.post('/createGroup', function (req, res) {
   })
 });
 
-// body: [uid (objectId), gid (objectId)]
+// body: [uid(objectId),gid(objectId)]
 // result: [“EXISTED” / “NOT FOUND”]
-router.post('/joinGroup', function (req, res) {
+router.post('/joinGroup',function (req, res) {
   var query = { _id: req.body.gid };
-  Group.find(query, function (err, groups) {
-    if (err) throw err
-    if (groups.length == 0) {
+  Group.find(query,function(err,groups) {
+    if(err) throw err
+    if(groups.length == 0) {
       return res.send("NOT FOUND");
     }
     else {
-      Join.findOne({ uid: req.body.uid, gid: req.body.gid, }, (err, join) => {
+      Join.findOne({uid: req.body.uid, gid: req.body.gid, },(err,join) => {
         if(join) {
           return res.send('EXISTED');
         }
@@ -102,7 +101,7 @@ router.post('/joinGroup', function (req, res) {
           read_at: 0
         });
         join_model.save(function (err) {
-          if (err) throw err;
+          if(err) throw err;
           else
             return res.send("EXISTED");
         })
@@ -116,15 +115,15 @@ router.post('/joinGroup', function (req, res) {
 router.post('/leaveGroup', function (req, res) {
   var query = { uid: req.body.uid, gid: req.body.gid };
   Join.remove(query, function (err, joins) {
-    if (err) return res.send("ERROR");
+    if(err) return res.send("ERROR");
     else return res.send("SUCCESS");
   });
 });
 
 // result: groups (array of object)
-router.get('/getAllGroup', function (req, res) {
+router.get('/getAllGroup',function(req,res) {
   
-  Group.find({}, function (err, groups) {
+  Group.find({},function(err,groups) {
     if (err) {
       throw err;
     }else {
@@ -137,7 +136,7 @@ router.get('/getAllGroup', function (req, res) {
 // Result: users [array of object]
 router.get('/getGroupUser', function (req, res) {
   result = []
-  Join.find({ gid: req.query.gid }, function (err, joins) {
+  Join.find({ gid: req.query.gid },function (err,joins) {
     if (err) {
       throw err
     } else {
@@ -151,21 +150,21 @@ router.get('/getGroupUser', function (req, res) {
 
 // query: [gid (objectId)]
 // result: messages (array of object)
-router.get("/getAllMessage", function (req, res) {
-  Message.find({ gid: req.query.gid }, function (err, messages) {
+router.get("/getAllMessage",function (req, res) {
+  Message.find({ gid: req.query.gid },function (err, messages) {
 
-    if (err) res.send('FAIL');
+    if(err) res.send('FAIL');
     else {
       let promises = [];
 
       promises = messages.map(message => {
-        return new Promise((resolve, reject) => {
-          User.findById(message.uid, (err, user) => {
-            if(err) {
+        return new Promise((resolve,reject) => {
+          User.findById(message.uid,(err,user) => {
+            if(err){
               console.error(err);
               reject()
             }
-            else {
+            else{
               message._doc.user = user;
               resolve();
             }
@@ -182,14 +181,14 @@ router.get("/getAllMessage", function (req, res) {
   });
 });
 
-// query: [uid (objectId) / gid (objectId)]
+// query: [uid(objectId) / gid(objectId)]
 // result: messages (array of object)
-router.get('/viewUnreadMessages', function (req, res) {
+router.get('/viewUnreadMessages',function (req,res) {
   var uid = req.query.uid;
   var gid = req.query.gid;
   var query = { uid: uid, gid: gid };
   var read_at;
-  Join.find(query, function (err, join) {
+  Join.find(query,function(err,join) {
     if (err) {
       throw err;
     }
@@ -197,14 +196,14 @@ router.get('/viewUnreadMessages', function (req, res) {
     
     if(join.length) {
       read_at = join[0].read_at;
-      Message.find({ send_at: { $gt: read_at }, gid: req.query.gid }).sort('send_at').exec(function (err, messages) {
+      Message.find({send_at: { $gt: read_at },gid: req.query.gid }).sort('send_at').exec(function(err, messages) {
         if (err) throw err
         else {
           let promises = [];
 
           promises = messages.map(message => {
-            return new Promise((resolve, reject) => {
-              User.findById(message.uid, (err, user) => {
+            return new Promise((resolve,reject) => {
+              User.findById(message.uid,(err,user) => {
                 if(err) {
                   console.error(err);
                   reject()
@@ -228,21 +227,21 @@ router.get('/viewUnreadMessages', function (req, res) {
   });
 });
 
-// body: [uid (objectId), gid (objectId), content (string)]
+// body: [uid(objectId),gid(objectId),content(string)]
 // result: [message]
-router.post('/sendMessage', function (req, res) {
+router.post('/sendMessage',function (req, res) {
   var query = Group.findOne({ gid: req.body.gid }).select('gid');
-  query.exec(function (err, group) {
-    if (err) throw err;
-    else {
+  query.exec(function (err,group) {
+    if(err) throw err;
+    else{
       var message_model = new Message({ uid: req.body.uid, gid: req.body.gid, content: req.body.content, send_at: Date.now() });
-      message_model.save(function (err, result) {
-        if (err) {
+      message_model.save(function(err,result) {
+        if(err) {
           res.send("ERROR");
           throw err
         }
         else {
-          User.find({ _id: message_model.uid }, (err, users) => {
+          User.find({ _id: message_model.uid },(err,users) => {
             let message = result._doc;
             message.user = users[0];
             Message.find({}).then(allMessages => {
@@ -255,19 +254,19 @@ router.post('/sendMessage', function (req, res) {
   })
 });
 
-// Body: [uid (objectId), gid (objectId)]
+// Body: [uid(objectId),gid(objectId)]
 // Result: [“SUCCESS” / “ERROR”]
-router.post("/setReadAt", function (req, res) {
+router.post("/setReadAt",function(req,res) {
 
-  Join.findOne({ uid: req.body.uid, gid: req.body.gid }, function (err, joins) {
+  Join.findOne({uid: req.body.uid,gid: req.body.gid },function(err,joins) {
 
-    if (err) throw err
-    else if (joins == null) return res.send("ERROR");
-    else {
+    if(err) throw err
+    else if(joins == null) return res.send("ERROR");
+    else{
       joins.set({ read_at: Date.now() });
       joins.save(function (err, update) {
-        if (err) throw err
-        else {
+        if(err) throw err
+        else{
           return res.send("SUCCESS");
         }
       });
@@ -275,21 +274,24 @@ router.post("/setReadAt", function (req, res) {
   });
 });
 
-router.get('/getMessageOrder', function (req, res) {
+router.get('/getMessageOrder',function(req,res) {
   Message.find({}).then(allMessages => {
     return res.send({messageOrder: allMessages.length});
   });
 });
 
-router.get("/allrooms",async (req,res) => {
-	const room = await Group.find({});
-	console.log(room.name)
-	return res.status(200).send(
-		JSON.stringify(
-			room.map(r=>{
-				return r.name;
-			})));
+
+router.get("/allrooms",async (req, res) => {
+  const room = await Group.find({});
+  console.log(room.name)
+  return res.status(200).send(
+    JSON.stringify(
+      room.map(r => {
+        return r.name;
+
+      })));
 });
+
 
 router.post("/allrooms", async (req, res) => {
   const room = await Group.find({ name: req.body.id });
@@ -308,6 +310,8 @@ router.put("/allrooms", async (req, res) => {
   if(room.length == 0){
     await Group.create({ name: req.body.id, user_id: [] });
     return res.status(201).send(JSON.stringify({ id: req.body.id }));
+
+
   }
   return res.status(200).send(JSON.stringify({ id: req.body.id }));
 
@@ -331,6 +335,23 @@ router.delete("/allrooms", async (req, res) => {
 
 router.get("/room/:id",async (req, res) => {
   const room = await Group.find({ name: req.params.id});
+  // var user;
+  // int i = 0;
+  // for( i = 0; i < join.length; i++){
+  //   user[i] = await User.find({ id: join.uid});
+  // };
+  // console.log(room.name)
+
+  // Join.aggregate([{
+  //   $lookup: {
+  //     from: "User",
+  //     localField: "uid",
+  //     foreignField: "id",
+  //     as: "userName"
+
+  //   }
+  // }])
+
   const join = await Join.find({ gid: room.id})//.project({'userName.name': 1});
   const user = await User.find(join.uid)
   // console.log(join.uid)
@@ -390,9 +411,10 @@ router.put("/room/:id", async (req, res) => {
         var user_model = new User({ name: req.body.user });
         user_model.save(function(err,userss) {
           if(err) throw err
-           
+            // return res.send({ "uid": userss.id });
         });
       }
+      // else return res.send({ "uid": users[0].id });
     });
 
   const join = await Join.find({ uid: user.id, gid: room.id})
@@ -408,6 +430,23 @@ router.put("/room/:id", async (req, res) => {
   console.log("200")
   return res.status(200).send("{}");
 });
+// router.put("/room/:ROOM_ID", async (req, res) => {
+//   const room = await Group.find({ name: req.params.ROOM_ID });
+//   // const user = await User.find({ name: req.body.user});
+//   const join = await Join.find({ uid: req.body.user, gid: req.params.ROOM_ID})
+
+//   if(join.length == 0){
+//     console.log("201")
+//     await Join.create({ uid: req.body.user, gid: req.params.ROOM_ID });
+//     return res.status(201).send("{}");
+
+
+//   }
+//   console.log("200")
+//   return res.status(200).send("{}");
+
+// });
+
 
 router.delete("/room/:id", async (req, res) => {
   const room = await Group.find({ name: req.params.id });
@@ -418,9 +457,10 @@ router.delete("/room/:id", async (req, res) => {
         var user_model = new User({ name: req.body.user });
         user_model.save(function(err,userss) {
           if(err) throw err
-            
+            // return res.send({ "uid": userss.id });
         });
       }
+      // else return res.send({ "uid": users[0].id });
     });
   const join = await Join.find({ uid: user[0].id, gid: room.id})
 
